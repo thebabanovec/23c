e4c3ca4d3dce5df5bb294cc2542f262fc1d8f4a1
thebabanovec <babanovec.6@gmail.com>
2026-03-27 20:16:52 +0100

    vsecko opraveno po velkym zmatku plus sand implementoane vice starnkove odpovedi

--- main.py
+++ main.py
@@ -1,110 +1,152 @@
-def confirmTime(txtNumero, txt: number):
-    while not (buttonPressed(confirmButton)):
-        displejTextu(txtNumero, txt)
-def displejTextu(txtNumero, txt):
-    if txt == None:
-        # mega genialni system jak mit hledani z listu i specialni texty v jednom
-        OLED.write_string_new_line(texty[level][scena][txtNumero])
-    else:
-        OLED.write_string_new_line(texty[level][scena][txt])
-def limit_otazky(pocet: number):
-    return pocet * LIMIT_RADEK + POCET_RADKU - pocet - 3 * pocet
-def buttonPressed(button: number):
-    if tinkercademy.ad_keyboard(button, buttons):
-        return True
-    else:
-        return False
-def answer_divider():
-    if len(odpovedi[level][scena][1]) + len(odpovedi[level][scena][2]) + len(odpovedi[level][scena][3]) + len(odpovedi[level][scena][4]) <= limit_otazky(4):
-        return [4, 0, 0, 0]
-    elif len(odpovedi[level][scena][1]) + len(odpovedi[level][scena][2]) + len(odpovedi[level][scena][3]) <= limit_otazky(3):
-        return [3, 1, 0, 0]
-    elif len(odpovedi[level][scena][1]) + len(odpovedi[level][scena][2]) <= limit_otazky(2):
-        if len(odpovedi[level][scena][3]) + len(odpovedi[level][scena][4]) <= limit_otazky(2):
-            return [2, 2, 0, 0]
-        else:
-            return [2, 1, 1, 0]
-    elif len(odpovedi[level][scena][2]) + len(odpovedi[level][scena][3]) + len(odpovedi[level][scena][4]) <= limit_otazky(3):
-        return [1, 3, 0, 0]
-    elif len(odpovedi[level][scena][3]) + len(odpovedi[level][scena][2]) <= limit_otazky(2):
-        return [1, 2, 1, 0]
-    elif len(odpovedi[level][scena][3]) + len(odpovedi[level][scena][4]) <= limit_otazky(2):
-        return [1, 1, 2, 0]
-    else:
-        return [1, 1, 1, 1]
-def answerTime():
-    global navigation2, changeDetector
-    navigation2 = 1
-    while not (buttonPressed(confirmButton) and odpovedi[level][scena][navigation2 - 1] != None):
-        if buttonPressed(downButton) and navigation2 < 5:
-            navigation2 = navigation2 + 1
-        elif buttonPressed(upButton) and navigation2 > 0:
-            navigation2 = navigation2 - 1
-        if navigation2 != changeDetector:
-            OLED.clear()
-        if navigation2 == 0:
-            displejTextu(0, None)
-        else:
-            displejOdpovedi(navigation2 - 1, [])
-        basic.pause(200)
-        changeDetector = navigation2
-    return navigation2 - 1
-changeDetector = 0
-navigation2 = 0
-POCET_RADKU = 0
-LIMIT_RADEK = 0
-odpovedi: List[List[List[str]]] = []
-texty: List[List[List[str]]] = []
-navigation = 0
-scena = 0
-# level 0
-# scena 1
-# scena 1.5
-# scena 2
-# level 1
-# scena 0
-# scena 1
-# scena 2
-level = 0
 comeon = "Smysl hry je zabit Caesara, tudiz odmitnout jeho vrazdu postrada smysl. Zkus to znovu. "
-texty = [[["Bibulus:  Zdravim priteli! Mam skvele zpravy - od dnesniho dne jsem konzulem!",
-            "Bibulus:  Oddanost jsi mi neprisahal pro nic za nic! Seber se a bez, nebo budes dalsi."],
-        ["Nemam cas, ani naladu na tve zertíky. Udelas to, nebo ne?"],
-        [""]],
-    [[""], [""], [""]]]
-# prvni text v scene je ten ktery se objevi pri otazkach
+texty = [ # prvni text v scene je ten ktery se objevi pri otazkach
+# level 0
+    [["Bibulus:  Zdravim priteli! Mam skvele zpravy - od dnesniho dne jsem konzulem!",# scena 1
+    "Bibulus:  Oddanost jsi mi neprisahal pro nic za nic! Seber se a bez, nebo budes dalsi."],
+    ["Nemam cas, ani naladu na tve zertíky. Udelas to, nebo ne?"],# scena 1.5
+    [""]],# scena 2
 # level 1
-# scena 1
-# scena 1.5
-odpovedi = [[["Cokoliv si prejete, pane Bibule. (vezmes si lahvicku)",
-            "Uhm… Pane, jste si jisty. To, o čem mluvite neni nic lehkovazného, je to VRAZDA!",
-            "NIKDY! Ja slouzim Caesarovi a nikdy bych mu neublizil!",
-            "Kdoze jste? V zivotě jsem vas nevidel…"],
-        [" Omlouvam se, pane, hned to provedu.",
-            " Ne. Nebudu zabijet pro ciziho cloveka."],
-        ["A02", "B02", "C02", "D02"]],
-    [["A10", "B10", "C10", "D10"],
-        ["A11", "B11", "C11", "D11"],
-        ["A12", "B12", "C12", "D12"]]]
+    [[""],# scena 1
+    [""],# scena 2
+    [""]]# scena 3
+]
+
+odpovedi = [
+# level 0
+    [["Cokoliv si prejete, pane Bibule. (vezmes si lahvicku)",# scena 1
+        "Uhm… Pane, jste si jisty. To, o čem mluvite neni nic lehkovazného, je to VRAZDA!",
+        "NIKDY! Ja slouzim Caesarovi a nikdy bych mu neublizil!",
+        "Kdoze jste? V zivotě jsem vas nevidel…"],
+    [" Omlouvam se, pane, hned to provedu.",# scena 1.5
+        " Ne. Nebudu zabijet pro ciziho cloveka."],
+    ["A02",# scena 2
+        "B02",
+        "C02",
+        "D02"]],
+# level 1
+    [["A10",# scena 1
+        "B10",
+        "C10",
+        "D10"],
+    ["A11",# scena 2
+        "B11",
+        "C11",
+        "D11"],
+    ["A12",# scena 3
+        "B12",
+        "C12",
+        "D12"]]
+]
+
 LIMIT_RADEK = 21
 POCET_RADKU = 8
+
 buttons = AnalogPin.P0
 upButton = ADKeys.A
 downButton = ADKeys.B
 confirmButton = ADKeys.D
+
+level = 0
+scena = 0
+
+
 OLED.init(128, 64)
-OLED.write_string("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-def displejOdpovedi(oznacenaOdpoved: number, deleniOdpovedi: any):
-    # predelat na OLED
+
+
+
+def button_pressed(button):
+    if tinkercademy.ad_keyboard(button, buttons):
+        return True
+    else:
+        return False
+
+def limit_otazky(pocet):
+    return pocet * LIMIT_RADEK + POCET_RADKU - pocet - 3 * pocet
+
+def answer_divider():
+    if odpovedi[level][scena][1].length + odpovedi[level][scena][2].length + odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(4):
+        return [4, 0, 0, 0]
+    elif odpovedi[level][scena][1].length + odpovedi[level][scena][2].length + odpovedi[level][scena][3].length <= limit_otazky(3):
+        return [3, 1, 0, 0]
+    elif odpovedi[level][scena][1].length + odpovedi[level][scena][2].length <= limit_otazky(2):
+        if odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(2):
+            return [2, 2, 0, 0]
+        else:
+            return [2, 1, 1, 0]
+
+    if odpovedi[level][scena][2].length + odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(3):
+        return [1, 3, 0, 0]
+    elif odpovedi[level][scena][3].length + odpovedi[level][scena][2].length <= limit_otazky(2):
+        return [1, 2, 1, 0]
+    elif odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(2):
+        return [1, 1, 2, 0]
+    else:
+        return [1, 1, 1, 1]
+
+def displej_textu(txtNumero, txt):
+    if txt == None:# mega genialni system jak mit hledani z listu i specialni texty v jednom
+        OLED.write_string_new_line(texty[level][scena][txtNumero])
+    else:
+        OLED.write_string_new_line(texty[level][scena][txt])
+
+def displej_odpovedi(oznacenaOdpoved, deleniOdpovedi):#[4, 0, 0, 0], 0
     pozice = 0
     temp = 0
-    while temp < oznacenaOdpoved:
-        temp = temp + deleniOdpovedi[pozice]
-        pozice = pozice + 1
-    for i in range(4):
+    while temp < oznacenaOdpoved + 1: #3
+        temp = temp + deleniOdpovedi[pozice] #4
+        pozice = pozice + 1 #1
+    
+    for i in range(temp-deleniOdpovedi[pozice],temp-1):#0,1,2,3
         if odpovedi[level][scena][i] == None:
             OLED.write_string_new_line("-----------")
         elif i == oznacenaOdpoved:
-            OLED.write_string_new_line("0. " + texty[level][scena][i])
+            OLED.write_string_new_line("0. " + odpovedi[level][scena][i])
         else:
             OLED.write_string_new_line(str(i + 1) + "." + odpovedi[level][scena][i])
+
+
+
+
+
+
+
+def answer_time():
+    changeDetector = 0
+    navigation = 0
+    while not (button_pressed(confirmButton) and odpovedi[level][scena][navigation - 1] != None):
+        if button_pressed(downButton) and navigation < 5:
+            navigation = navigation + 1
+        elif button_pressed(upButton) and navigation > 0:
+            navigation = navigation - 1
+
+        if navigation != changeDetector:
+            OLED.clear()
+            if navigation == 0:
+                displej_textu(0, None)
+            else:
+                displej_odpovedi(navigation - 1, [])
+        basic.pause(200)
+        changeDetector = navigation
+    return navigation - 1
+
+def confirm_time(txtNumero, txt):
+    while not (button_pressed(confirmButton)):
+        displej_textu(txtNumero, txt)
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
--- main.ts
+++ main.ts
@@ -1,30 +1,43 @@
-function confirmTime(txtNumero: number, txt: number) {
-    while (!buttonPressed(confirmButton)) {
-        displejTextu(txtNumero, txt)
-    }
-}
-
-function displejTextu(txtNumero: number, txt: number) {
-    if (txt == null) {
-        //  mega genialni system jak mit hledani z listu i specialni texty v jednom
-        OLED.writeStringNewLine(texty[level][scena][txtNumero])
-    } else {
-        OLED.writeStringNewLine(texty[level][scena][txt])
-    }
-    
-}
-
-function limit_otazky(pocet: number): number {
-    return pocet * LIMIT_RADEK + POCET_RADKU - pocet - 3 * pocet
-}
-
-function buttonPressed(button: number): boolean {
+let comeon = "Smysl hry je zabit Caesara, tudiz odmitnout jeho vrazdu postrada smysl. Zkus to znovu. "
+let texty = [[["Bibulus:  Zdravim priteli! Mam skvele zpravy - od dnesniho dne jsem konzulem!", "Bibulus:  Oddanost jsi mi neprisahal pro nic za nic! Seber se a bez, nebo budes dalsi."], ["Nemam cas, ani naladu na tve zertíky. Udelas to, nebo ne?"], [""]], [[""], [""], [""]]]
+//  prvni text v scene je ten ktery se objevi pri otazkach
+//  level 0
+//  scena 1
+//  scena 1.5
+//  scena 2
+//  level 1
+//  scena 1
+//  scena 2
+//  scena 3
+let odpovedi = [[["Cokoliv si prejete, pane Bibule. (vezmes si lahvicku)", "Uhm… Pane, jste si jisty. To, o čem mluvite neni nic lehkovazného, je to VRAZDA!", "NIKDY! Ja slouzim Caesarovi a nikdy bych mu neublizil!", "Kdoze jste? V zivotě jsem vas nevidel…"], [" Omlouvam se, pane, hned to provedu.", " Ne. Nebudu zabijet pro ciziho cloveka."], ["A02", "B02", "C02", "D02"]], [["A10", "B10", "C10", "D10"], ["A11", "B11", "C11", "D11"], ["A12", "B12", "C12", "D12"]]]
+//  level 0
+//  scena 1
+//  scena 1.5
+//  scena 2
+//  level 1
+//  scena 1
+//  scena 2
+//  scena 3
+let LIMIT_RADEK = 21
+let POCET_RADKU = 8
+let buttons = AnalogPin.P0
+let upButton = ADKeys.A
+let downButton = ADKeys.B
+let confirmButton = ADKeys.D
+let level = 0
+let scena = 0
+OLED.init(128, 64)
+function button_pressed(button: number): boolean {
     if (tinkercademy.ADKeyboard(button, buttons)) {
         return true
     } else {
         return false
     }
     
+}
+
+function limit_otazky(pocet: number): number {
+    return pocet * LIMIT_RADEK + POCET_RADKU - pocet - 3 * pocet
 }
 
 function answer_divider(): number[] {
@@ -39,7 +52,9 @@ function answer_divider(): number[] {
             return [2, 1, 1, 0]
         }
         
-    } else if (odpovedi[level][scena][2].length + odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(3)) {
+    }
+    
+    if (odpovedi[level][scena][2].length + odpovedi[level][scena][3].length + odpovedi[level][scena][4].length <= limit_otazky(3)) {
         return [1, 3, 0, 0]
     } else if (odpovedi[level][scena][3].length + odpovedi[level][scena][2].length <= limit_otazky(2)) {
         return [1, 2, 1, 0]
@@ -51,81 +66,69 @@ function answer_divider(): number[] {
     
 }
 
-function answerTime(): number {
-    
-    navigation2 = 1
-    while (!(buttonPressed(confirmButton) && odpovedi[level][scena][navigation2 - 1] != null)) {
-        if (buttonPressed(downButton) && navigation2 < 5) {
-            navigation2 = navigation2 + 1
-        } else if (buttonPressed(upButton) && navigation2 > 0) {
-            navigation2 = navigation2 - 1
-        }
-        
-        if (navigation2 != changeDetector) {
-            OLED.clear()
-        }
-        
-        if (navigation2 == 0) {
-            displejTextu(0, null)
-        } else {
-            displejOdpovedi(navigation2 - 1, [])
-        }
-        
-        basic.pause(200)
-        changeDetector = navigation2
+function displej_textu(txtNumero: number, txt: number) {
+    if (txt == null) {
+        //  mega genialni system jak mit hledani z listu i specialni texty v jednom
+        OLED.writeStringNewLine(texty[level][scena][txtNumero])
+    } else {
+        OLED.writeStringNewLine(texty[level][scena][txt])
     }
-    return navigation2 - 1
+    
 }
 
-let changeDetector = 0
-let navigation2 = 0
-let POCET_RADKU = 0
-let LIMIT_RADEK = 0
-let odpovedi : string[][][] = []
-let texty : string[][][] = []
-let navigation = 0
-let scena = 0
-//  level 0
-//  scena 1
-//  scena 1.5
-//  scena 2
-//  level 1
-//  scena 0
-//  scena 1
-//  scena 2
-let level = 0
-let comeon = "Smysl hry je zabit Caesara, tudiz odmitnout jeho vrazdu postrada smysl. Zkus to znovu. "
-texty = [[["Bibulus:  Zdravim priteli! Mam skvele zpravy - od dnesniho dne jsem konzulem!", "Bibulus:  Oddanost jsi mi neprisahal pro nic za nic! Seber se a bez, nebo budes dalsi."], ["Nemam cas, ani naladu na tve zertíky. Udelas to, nebo ne?"], [""]], [[""], [""], [""]]]
-//  prvni text v scene je ten ktery se objevi pri otazkach
-//  level 1
-//  scena 1
-//  scena 1.5
-odpovedi = [[["Cokoliv si prejete, pane Bibule. (vezmes si lahvicku)", "Uhm… Pane, jste si jisty. To, o čem mluvite neni nic lehkovazného, je to VRAZDA!", "NIKDY! Ja slouzim Caesarovi a nikdy bych mu neublizil!", "Kdoze jste? V zivotě jsem vas nevidel…"], [" Omlouvam se, pane, hned to provedu.", " Ne. Nebudu zabijet pro ciziho cloveka."], ["A02", "B02", "C02", "D02"]], [["A10", "B10", "C10", "D10"], ["A11", "B11", "C11", "D11"], ["A12", "B12", "C12", "D12"]]]
-LIMIT_RADEK = 21
-POCET_RADKU = 8
-let buttons = AnalogPin.P0
-let upButton = ADKeys.A
-let downButton = ADKeys.B
-let confirmButton = ADKeys.D
-OLED.init(128, 64)
-OLED.writeString("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
-function displejOdpovedi(oznacenaOdpoved: number, deleniOdpovedi: string) {
-    //  predelat na OLED
+function displej_odpovedi(oznacenaOdpoved: number, deleniOdpovedi: any) {
+    // [4, 0, 0, 0], 0
     let pozice = 0
     let temp = 0
-    while (temp < oznacenaOdpoved) {
+    while (temp < oznacenaOdpoved + 1) {
+        // 3
         temp = temp + deleniOdpovedi[pozice]
+        // 4
         pozice = pozice + 1
     }
-    for (let i = 0; i < 4; i++) {
+    // 1
+    for (let i = temp - deleniOdpovedi[pozice]; i < temp - 1; i++) {
+        // 0,1,2,3
         if (odpovedi[level][scena][i] == null) {
             OLED.writeStringNewLine("-----------")
         } else if (i == oznacenaOdpoved) {
-            OLED.writeStringNewLine("0. " + texty[level][scena][i])
+            OLED.writeStringNewLine("0. " + odpovedi[level][scena][i])
         } else {
             OLED.writeStringNewLine("" + (i + 1) + "." + odpovedi[level][scena][i])
         }
         
+    }
+}
+
+function answer_time(): number {
+    let changeDetector = 0
+    let navigation = 0
+    while (!(button_pressed(confirmButton) && odpovedi[level][scena][navigation - 1] != null)) {
+        if (button_pressed(downButton) && navigation < 5) {
+            navigation = navigation + 1
+        } else if (button_pressed(upButton) && navigation > 0) {
+            navigation = navigation - 1
+        }
+        
+        if (navigation != changeDetector) {
+            OLED.clear()
+            if (navigation == 0) {
+                displej_textu(0, null)
+            } else {
+                displej_odpovedi(navigation - 1, [])
+            }
+            
+        }
+        
+        basic.pause(200)
+        changeDetector = navigation
+    }
+    return navigation - 1
+}
+
+function confirm_time(txtNumero: number, txt: number) {
+    while (!button_pressed(confirmButton)) {
+        displej_textu(txtNumero, txt)
     }
 }
 
